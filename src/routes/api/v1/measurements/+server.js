c1589a5cea352f2e62579a749764aaaf1fec7a4a
szentmik <szentmik@gmail.com>
2026-02-09 23:15:36 +0100

    fix: add error handling for invalid year and numeric value in measurement functions

--- src/routes/api/v1/measurements/+server.js
+++ src/routes/api/v1/measurements/+server.js
@@ -10,6 +10,8 @@ export const GET = async (event) => {
     } catch (err) {
         if (err.message === "Unauthorized") return json({ error: "You have to login" }, { status: 401 });
         if (err.message === "Invalid month") return json({ error: "Invalid month" }, { status: 400 });
+        if (err.message === "Invalid year") return json({ error: "Invalid year" }, { status: 400 });
+
         return json({ error: "Failed to get the data" }, { status: 500 });
     }
 }
@@ -21,6 +23,7 @@ export const POST = async (event) => {
     } catch (err) {
         if (err.message === "Unauthorized") return json({ error: "Unauthorized" }, { status: 401 });
         if (err.message === "Today already measured, not allowed to modify") return json({ error: "We have already values for today" }, { status: 409 });
+        if (err.message === "Invalid numeric value") return json({ error: "Invalid numeric value" }, { status: 400 });
         return json({ error: "Connection failed." }, { status: 500 });
     }
 }
