fc90fa68d3eaff4b64757f588a73b0f7f37e7afd
szentmik <szentmik@gmail.com>
2026-02-09 19:34:08 +0100

    refactor: streamline error handling and improve measurement retrieval logic

--- src/lib/server/controllers/measuresController.js
+++ src/lib/server/controllers/measuresController.js
@@ -1,108 +1,82 @@
-import { json } from "@sveltejs/kit";
-import { measureServices } from "../services/measureServices";
+import { json } from '@sveltejs/kit'
+import { measureServices } from '../services/measureServices'
 
 export const getAllMeasurements = async ({ url, locals }) => {
-    try {
-        //        const year = parseInt(url.searchParams.get("year")) || new Date().getFullYear();
-        //        const month = parseInt(url.searchParams.get("month")) || new Date().getMonth() + 1;
-        const yearParam = Number(url.searchParams.get("year"));
-        const monthParam = Number(url.searchParams.get("month"));
-        const year = Number.isInteger(yearParam) ? yearParam : new Date().getFullYear();
-        const month = Number.isInteger(monthParam) ? monthParam : new Date().getMonth() + 1;
-        if (month < 1 || month > 12) {
-            return json({ error: "Invalid month" }, { status: 400 });
-        }
 
-        const userId = (await locals.auth())?.userId;
-        if (!userId) return json({ error: "Unauthorized" }, { status: 401 });
+  const yearParam = Number(url.searchParams.get('year'))
+  const monthParam = Number(url.searchParams.get('month'))
+  const year = Number.isInteger(yearParam) ? yearParam : new Date().getFullYear()
+  const month = Number.isInteger(monthParam) ? monthParam : new Date().getMonth() + 1
 
-        const result = await measureServices.getComparisonDataByMonth(year, month);
+  if (month < 1 || month > 12) throw new Error('Invalid month')
 
-        return json(result, { status: 200 });
+  const userId = (await locals.auth())?.userId
 
-    } catch (err) {
-        console.error("Request failed", err);
+  if (!userId) throw new Error('Unauthorized')
 
-        return json({ error: "Connection error" }, { status: 500 });
-    }
-};
+  return await measureServices.getComparisonDataByMonth(year, month)
+}
 
 export const getMeasurementsByUserId = async ({ locals }) => {
-    try {
-        const userId = (await locals.auth())?.userId;
-        if (!userId) return json({ error: "Unauthorized" }, { status: 401 });
 
-        const result = await measureServices.getByUserId(userId);
+  const userId = (await locals.auth())?.userId
 
-        return json(result, { status: 200 });
+  if (!userId) throw new Error("Unauthorized");
 
-    } catch (err) {
-        console.error("Request failed", err);
-        return json({ error: "Connection error" }, { status: 500 });
-    }
-};
+  return await measureServices.getByUserId(userId)
+
+}
 
 export const addNewManualData = async ({ request, locals }) => {
-    try {
-        const userId = (await locals.auth())?.userId;
-        if (!userId) return json({ error: "Unauthorized" }, { status: 401 });
+  const userId = (await locals.auth())?.userId
+  if (!userId) throw new Error("Unauthorized");
 
-        const body = await request.json();
-        const phValue = body.phValue != null ? Number(body.phValue) : null;
-        const chlorValue = body.chlorValue != null ? Number(body.chlorValue) : null;
-        const totalClValue = body.totalClValue != null ? Number(body.totalClValue) : null;
-        let gebClValue = null;
 
-        if (chlorValue !== null && totalClValue !== null) {
-            gebClValue = Number((totalClValue - chlorValue).toFixed(2));
-        }
+  const body = await request.json()
+  const phValue = body.phValue != null ? Number(body.phValue) : null
+  const chlorValue = body.chlorValue != null ? Number(body.chlorValue) : null
+  const totalClValue = body.totalClValue != null ? Number(body.totalClValue) : null
+  let gebClValue = null
 
-        const data = {
-            phValue,
-            chlorValue,
-            totalClValue,
-            gebClValue,
-        }
+  if (chlorValue !== null && totalClValue !== null) {
+    gebClValue = Number((totalClValue - chlorValue).toFixed(2))
+  }
 
-        const result = await measureServices.upsertManual(data, userId);
+  const data = {
+    phValue,
+    chlorValue,
+    totalClValue,
+    gebClValue
+  }
 
-        return json(result);
-
-    } catch (err) {
-        console.error(err.message);
-        return json({ error: err.message }, { status: 400 });
-    }
-};
+  return await measureServices.upsertManual(data, userId)
+}
 
 export const addNewSystemData = async ({ request, locals }) => {
-    try {
-        const userId = (await locals.auth())?.userId;
-        if (!userId) return json({ error: "Unauthorized" }, { status: 401 });
-        const body = await request.json();
 
-        const phValue = body.sysPhValue ? Number(body.sysPhValue) : null;
-        const chlorValue = body.sysChlorValue ? Number(body.sysChlorValue) : null;
-        const redoxValue = body.sysRedoxValue ? Number(body.sysRedoxValue) : null;
-        const waterTemp = body.sysWaterTemp ? Number(body.sysWaterTemp) : null;
-        const flow = body.sysFlow ? Number(body.sysFlow) : null;
-        const filterBackwash = body.sysFilterBackwash === true || body.sysFilterBackwash === "true";
+  const userId = (await locals.auth())?.userId
+  if (!userId) throw new Error("Unauthorized");
 
-        const data = {
-            phValue,
-            chlorValue,
-            redoxValue,
-            waterTemp,
-            flow,
-            filterBackwash,
-            userId,
-        }
+  const body = await request.json()
 
-        const result = await measureServices.insertSystem(data);
+  const phValue = body.sysPhValue != null ? Number(body.sysPhValue) : null
+  const chlorValue = body.sysChlorValue != null ? Number(body.sysChlorValue) : null
+  const redoxValue = body.sysRedoxValue != null ? Number(body.sysRedoxValue) : null
+  const waterTemp = body.sysWaterTemp != null ? Number(body.sysWaterTemp) : null
+  const flow = body.sysFlow != null ? Number(body.sysFlow) : null
+  const filterBackwash =
+    body.sysFilterBackwash === true || body.sysFilterBackwash === 'true'
 
-        return json(result);
+  const data = {
+    phValue,
+    chlorValue,
+    redoxValue,
+    waterTemp,
+    flow,
+    filterBackwash,
+    userId
+  }
 
-    } catch (err) {
-        console.error(err.message);
-        return json({ error: err.message }, { status: 400 });
-    }
-};
+  return await measureServices.insertSystem(data)
+
+}
--- src/routes/api/v1/measurements/+server.js
+++ src/routes/api/v1/measurements/+server.js
@@ -1,13 +1,26 @@
-import { measureServices } from "$lib/server/services/measureServices";
+
+import { addNewManualData, getAllMeasurements } from "$lib/server/controllers/measuresController";
 import { json } from "@sveltejs/kit";
 
-export const GET = async () => {
+export const GET = async (event) => {
     try {
-        const data = await measureServices.getAll();
+        const data = await getAllMeasurements(event);
         return json(data);
 
-    } catch (error) {
-        console.error("Failed to get the data", error);
-        return json({ error: "Failed to get th data" }, { status: 500 });
+    } catch (err) {
+        if (err.message === "Unauthorized") return json({ error: "You have to login" }, { status: 401 });
+        if (err.message === "Invalid month") return json({ error: "Invalis month" }, { status: 400 });
+        return json({ error: "Failed to get the data" }, { status: 500 });
+    }
+}
+
+export const POST = async (event) => {
+    try {
+        const result = await addNewManualData(event);
+        return json(result);
+    } catch (err) {
+        if (err.message === "Unauthorized") return json({ error: "Unauthorized" }, { status: 401 });
+        if (err.message === "Today already measured, not allowed to modify") return json({ error: "We have already values for today" }, { status: 409 });
+        return json({ error: "Connection failed." }, { status: 500 });
     }
 }
