fc65687ed87e58ca05cd578accbb779fa1416510
szentmik <szentmik@gmail.com>
2026-02-10 20:29:37 +0100

    fix: format JSON response status and improve error handling in measurement endpoints

--- src/routes/api/v1/measurements/+server.js
+++ src/routes/api/v1/measurements/+server.js
@@ -5,7 +5,7 @@ import { json } from "@sveltejs/kit";
 export const GET = async (event) => {
     try {
         const data = await getAllMeasurements(event);
-        return json(data, {status: 200});
+        return json(data, { status: 200 });
 
     } catch (err) {
         if (err.message === "Unauthorized") return json({ error: "Unauthorized" }, { status: 401 });
@@ -19,11 +19,13 @@ export const GET = async (event) => {
 export const POST = async (event) => {
     try {
         const result = await addNewManualData(event);
-        return json(result, {status: 201});
+        return json(result, { status: 201 });
     } catch (err) {
         if (err.message === "Unauthorized") return json({ error: "Unauthorized" }, { status: 401 });
         if (err.message === "Today already measured, not allowed to modify") return json({ error: "We have already values for today" }, { status: 409 });
         if (err.message === "Invalid numeric value") return json({ error: "Invalid numeric value" }, { status: 400 });
-        return json({ error: "Connection failed." }, { status: 500 });
+
+        console.error("Connection error", err);
+        return json({ error: "Connection error." }, { status: 500 });
     }
 }
--- src/routes/api/v1/measurements/addsystem/+server.js
+++ src/routes/api/v1/measurements/addsystem/+server.js
@@ -7,7 +7,8 @@ export const POST = async (event) => {
         return json(result, {status: 201});
     } catch (err) {
         if (err.message === "Unauthorized") return json({error: "Unauthorized"}, {status: 401});
-        if (err.message === "Today every values were added") return json({error: "Today every values were added"}, {status: 409})
+        if (err.message === "Invalid numeric value") return json({error: "Invalid numeric value"}, {status:400});
+        if (err.message === "Today every values were added") return json({error: "We have already values for today"}, {status: 409})
 
         console.error("Connection error", err);
         return json({error: "Connection error"}, {status: 500});
