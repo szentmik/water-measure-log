dd298684b69584c4aafd7e0d367da812b00af8c0
szentmik <szentmik@gmail.com>
2026-02-18 19:43:13 +0100

    refactor: remove unused Clerk buttons and clean up styles in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { SignInButton, SignUpButton } from "svelte-clerk";
+  
 </script>
 
 <div>
@@ -8,10 +8,7 @@
         The swimming pool watermeasurement log application for Hotels and
         wellness.
     </p>
-    <div class="clerk-btn">
-        <SignInButton />
-        <SignUpButton />
-    </div>
+  
 </div>
 
 <style>
@@ -27,30 +24,4 @@
         }
     }
 
-    .clerk-btn{
-        display: flex;
-        justify-content: center;
-        gap:.75rem;
-    }
-
-    .clerk-btn :global(button) {
-        background-color: var(--primary);
-        color: var(--bg);
-        cursor: pointer;
-        border-style: none;
-        border-radius: .75rem;
-        box-shadow: .1rem .1rem 1rem var(--text-muted);
-        font-size: 1.25rem;
-        font-weight: bolder;
-        padding: 0.5rem 1rem;
-        text-shadow: 0.2rem var(--text);
-    }
-
-    .clerk-btn :global(button):nth-of-type(2) {
-        background-color: var(--secondary);
-    }
-
-    .clerk-btn :global(button):hover {
-        background-color: var(--info);
-    }
 </style>
